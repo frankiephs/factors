b70885d801f8fe1acf2aa03252e12e435029c39a
Your Name <fb23419@student.phs.school.nz>
2024-02-24 21:30:03 +1300

    first commit on factors

--- C_01.py
+++ C_01.py
@@ -3,6 +3,6 @@ def instructions():
     print("Instructions are displayed")
 
 want_instructions = input("Press <enter> to read or any key to continue: ")
-
+    
 if want_instructions == "":
     instructions()
